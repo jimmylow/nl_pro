db56b23c7c5a71119a8e7895cfbc4a37e6c1c707
jimmylow <jimmylow@wavelet.net>
2018-03-25 22:49:35 +0800

    #FG Stock Movement Report: Add show invoice qty

--- prod_rpt/fg_moverpt.php
+++ prod_rpt/fg_moverpt.php
@@ -1,39 +1,38 @@
 <?php
-    include("../Setting/Configifx.php");
+	include("../Setting/Configifx.php");
 	include("../Setting/Connection.php");
 	$var_loginid = $_SESSION['sid'];
+	if($var_loginid == "") {
+    	echo "<script>";
+    	echo "alert('Not Log In to the system');";
+    	echo "</script>";
     
-    if($var_loginid == "") { 
-      echo "<script>";   
-      echo "alert('Not Log In to the system');"; 
-      echo "</script>"; 
+    	echo "<script>";
+   		echo 'top.location.href = "../index.php"';
+    	echo "</script>";
+	} else {
+    	$var_menucode = $_GET['menucd'];
+    	include("../Setting/ChqAuth.php");
+	}
 
-      echo "<script>";
-      echo 'top.location.href = "../index.php"';
-      echo "</script>";
-    } else {
-      $var_menucode = $_GET['menucd'];
-      include("../Setting/ChqAuth.php");
-    }
+	$datefrom = date("01-m-Y");
+	$dateto = date("t-m-Y");
 
-    $datefrom = date("01-m-Y");
-    $dateto = date("t-m-Y");
-    
-    if (isset($_POST['Submit'])){ 
-     if ($_POST['Submit'] == "Print") {
-		$fdte = date("Y-m-d", strtotime($_POST['rptofdte']));
-     	$tdte = date("Y-m-d", strtotime($_POST['rptotdte']));
-     	$datefrom = date("d-m-Y", strtotime($_POST['rptofdte']));
-     	$dateto = date("d-m-Y", strtotime($_POST['rptotdte']));
-     	$fjid = $_POST['selfjid'];
-     	$tjid = $_POST['seltjid'];
-     	$show0Balance = $_POST['chkShowBalance'];
-     	$nodata = 0;
-     	
-     	#----------------Prepare Temp Table For Printing -----------------------------------
-     	$sql  = " Delete From tmpfgmove where usernm = '$var_loginid'";
+if (isset($_POST['Submit'])){
+    if ($_POST['Submit'] == "Print") {
+        $fdte = date("Y-m-d", strtotime($_POST['rptofdte']));
+        $tdte = date("Y-m-d", strtotime($_POST['rptotdte']));
+        $datefrom = date("d-m-Y", strtotime($_POST['rptofdte']));
+        $dateto = date("d-m-Y", strtotime($_POST['rptotdte']));
+        $fjid = $_POST['selfjid'];
+        $tjid = $_POST['seltjid'];
+        $show0Balance = $_POST['chkShowBalance'];
+        $nodata = 0;
+        
+        #----------------Prepare Temp Table For Printing -----------------------------------
+        $sql  = " Delete From tmpfgmove where usernm = '$var_loginid'";
         mysql_query($sql) or die("Unable To Prepare Temp Table For Printing".mysql_error());
-	
+        
         $prod_code_csv = "";
         if (empty($show0Balance)) {
             $sql  = "SELECT sum(totalqty), item_code";
@@ -42,122 +41,157 @@
             $sql .= " and item_code between '$fjid' and '$tjid'";
             $sql .= " group by item_code having sum(totalqty) != 0";
             
-            $rs_result = mysql_query($sql);            
+            $rs_result = mysql_query($sql);
             while ($row = mysql_fetch_assoc($rs_result)) {
                 if (!empty($prod_code_csv)) {
-                    $prod_code_csv .= ",";   
+                    $prod_code_csv .= ",";
                 }
-                $prod_code_csv .= "'" .$row['item_code']. "'";               
-            }   
+                $prod_code_csv .= "'" .$row['item_code']. "'";
+            }
             if (empty($prod_code_csv)) {
                 /* echo "<script>";
-                echo "alert('No data for the report!');";
-                echo "</script>"; */
+                 echo "alert('No data for the report!');";
+                 echo "</script>"; */
                 $nodata = 1;
-            }       
+            }
         }
         
-		$shardSize = 8000;
-	 	$sqliq = "";		   			
-	 	$k = 0;	
-	 	if ($nodata==0) {
-		$sql  = "SELECT sum(totalqty), item_code";
-		$sql .= " FROM fg_tran";
-    	$sql .= " where trxdate < '$fdte'";
-    	$sql .= " and item_code between '$fjid' and '$tjid'";
-    	if (empty($show0Balance)) {
-    	    $sql .= " and item_code in ($prod_code_csv)";
-    	}
-    	$sql .= " group by item_code";
-		$rs_result = mysql_query($sql);
-
-		while ($row = mysql_fetch_assoc($rs_result)) { 
-		   	$bfqty     = $row['sum(totalqty)'];
-		   	$prod_code = $row['item_code'];       	
-       		if (empty($bfqty)){$bfqty = 0;}        	
-       		
-       		if ($bfqty <> 0){
-        		if ($k % $shardSize == 0) {
-        			if ($k != 0) {	  
-        	    		mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
-        			}
-        			$sqliq = 'Insert Into tmpfgmove (usernm, prodcd, bfqty) Values ';
-    			}
-   				$sqliq .= (($k % $shardSize == 0) ? '' : ', ') . "('$var_loginid', '$prod_code', '$bfqty')";
-			 	$k = $k + 1;	
-			}		
-		 }
-		 if (!empty($sqliq)){
-		 	mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
-		 }
-	
-		 $shardSize = 8000;
-	  	 $sqliq = "";		   			
-	 	 $k = 0;	
-		 $sql  = "SELECT sum(totalqty), tran_type, item_code";
-		 $sql .= " FROM fg_tran";
-    	 $sql .= " where trxdate between '$fdte' and '$tdte'";
-    	 $sql .= " and item_code between '$fjid' and '$tjid'";
-    	 if (empty($show0Balance)) {
-    	     $sql .= " and item_code in (".$prod_code_csv.")";
-    	 }
-    	 $sql .= " group by tran_type, item_code";
-		 $rs_result = mysql_query($sql);
-
-		while ($row = mysql_fetch_assoc($rs_result)) { 
-		   	$trqty     = $row['sum(totalqty)'];
-		   	$trty      = $row['tran_type'];
-		   	$prod_code = $row['item_code'];       	
-       		if (empty($trqty)){$trqty = 0;}
-       		
-			$oqty = 0;   $iqty = 0;  $rqty = 0;  $aqty = 0;
-			switch($trty){
-			case "ISS":
-				$oqty = 0;   $iqty = $trqty;  $rqty = 0;  $aqty = 0;
-				break;
-			case "OPB":
-				$oqty = $trqty;   $iqty = 0;  $rqty = 0;  $aqty = 0;
-				break;
-			case "REC":
-				$oqty = 0;   $iqty = 0;  $rqty = $trqty;  $aqty = 0;
-				break;
-			case "ADJ":
-				$oqty = 0;   $iqty = 0;  $rqty = 0;  $aqty = $trqty;
-				break;
-			}        	
-       		
-        	if ($k % $shardSize == 0) {
-        		if ($k != 0) {	  
-        	   		mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
-        		}
-        		$sqliq = 'Insert Into tmpfgmove (usernm, prodcd, openqty, issqty, recqty, adjqty) Values ';
-    		}
-   			$sqliq .= (($k % $shardSize == 0) ? '' : ', ') . "('$var_loginid', '$prod_code', '$oqty', '$iqty',
-   			                                                   '$rqty', '$aqty')";
-			$k = $k + 1;	
-					
-		 }
-		 if (!empty($sqliq)){
-		 	mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
-		 }
-	 	} // end if nodata=0
-	 	
-     	 $fname = "fg_moverpt.rptdesign&__title=myReport"; 
-     	 $fname .= "&fd=".$fdte."&td=".$tdte;
-     	 $fname .= "&fpr=".$fjid."&tpr=".$tjid;
-     	 
-         $dest = "http://".$var_prtserver.":8080/birt-viewer/frameset?__report=".$fname."&usernm=".$var_loginid."&dbsel=".$varrpturldb;
-         $dest .= urlencode(realpath($fname));
-
-         //header("Location: $dest" );
-         echo "<script language=\"javascript\">window.open('".$dest."','name','height=1000,width=1000,left=200,top=200');</script>";
-         //$backloc = "../prod_rpt/fg_moverpt.php?menucd=".$var_menucode;
-       	 //echo "<script>";
-       	 //echo 'location.replace("'.$backloc.'")';
-       	 //echo "</script>";  
-	 	
-     }
-    } 
+        $shardSize = 8000;
+        $sqliq = "";
+        $k = 0;
+        if ($nodata==0) {
+            $sql  = "SELECT sum(totalqty), item_code";
+            $sql .= " FROM fg_tran";
+            $sql .= " where trxdate < '$fdte'";
+            $sql .= " and item_code between '$fjid' and '$tjid'";
+            if (empty($show0Balance)) {
+                $sql .= " and item_code in ($prod_code_csv)";
+            }
+            $sql .= " group by item_code";
+            $rs_result = mysql_query($sql);
+            
+            while ($row = mysql_fetch_assoc($rs_result)) {
+                $bfqty     = $row['sum(totalqty)'];
+                $prod_code = $row['item_code'];
+                if (empty($bfqty)){$bfqty = 0;}
+                
+                if ($bfqty <> 0){
+                    if ($k % $shardSize == 0) {
+                        if ($k != 0) {
+                            mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+                        }
+                        $sqliq = 'Insert Into tmpfgmove (usernm, prodcd, bfqty) Values ';
+                    }
+                    $sqliq .= (($k % $shardSize == 0) ? '' : ', ') . "('$var_loginid', '$prod_code', '$bfqty')";
+                    $k = $k + 1;
+                }
+            }
+            if (!empty($sqliq)){
+                mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+            }
+            
+            $shardSize = 8000;
+            $sqliq = "";
+            $k = 0;
+            $sql  = "SELECT sum(totalqty), tran_type, item_code";
+            $sql .= " FROM fg_tran";
+            $sql .= " where trxdate between '$fdte' and '$tdte'";
+            $sql .= " and item_code between '$fjid' and '$tjid'";
+            if (empty($show0Balance)) {
+                $sql .= " and item_code in (".$prod_code_csv.")";
+            }
+            $sql .= " group by tran_type, item_code";
+            $rs_result = mysql_query($sql);
+            
+            while ($row = mysql_fetch_assoc($rs_result)) {
+                $trqty     = $row['sum(totalqty)'];
+                $trty      = $row['tran_type'];
+                $prod_code = $row['item_code'];
+                if (empty($trqty)){$trqty = 0;}
+                
+                $oqty = 0;   $iqty = 0;  $rqty = 0;  $aqty = 0; $invqty = 0;
+                switch($trty){
+                    case "ISS":
+                        $oqty = 0;   $iqty = $trqty;  $rqty = 0;  $aqty = 0;
+                        break;
+                    case "OPB":
+                        $oqty = $trqty;   $iqty = 0;  $rqty = 0;  $aqty = 0;
+                        break;
+                    case "REC":
+                        $oqty = 0;   $iqty = 0;  $rqty = $trqty;  $aqty = 0;
+                        break;
+                    case "ADJ":
+                        $oqty = 0;   $iqty = 0;  $rqty = 0;  $aqty = $trqty;
+                        break;
+                }
+                
+                if ($k % $shardSize == 0) {
+                    if ($k != 0) {
+                        mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+                    }
+                    $sqliq = 'Insert Into tmpfgmove (usernm, prodcd, openqty, issqty, recqty, adjqty, invoiceqty) Values ';
+                }
+                $sqliq .= (($k % $shardSize == 0) ? '' : ', ') . "('$var_loginid', '$prod_code', '$oqty', '$iqty',
+   			                                                   '$rqty', '$aqty', '$invqty')";
+                $k = $k + 1;
+                
+            }
+            if (!empty($sqliq)){
+                mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+            }
+            
+            // invoice qty
+            $shardSize = 8000;
+            $sqliq = "";
+            $k = 0;
+            $sql  = "select sum(si.qtys) as total_qty, si.sysprocd";
+            $sql .= " from ship_invdet si";
+            $sql .= " inner join ship_invmas s ON s.invno = si.invno";
+            $sql .= " where sysprocd != ''";
+            $sql .= " and s.invdte between '$fdte' and '$tdte'";
+            $sql .= " and si.sysprocd between '$fjid' and '$tjid'";
+            $sql .= " group by si.sysprocd";
+            $rs_result = mysql_query($sql);
+            
+            $oqty = 0;   $iqty = 0;  $rqty = 0;  $aqty = 0;
+            while ($row = mysql_fetch_assoc($rs_result)) {
+                $trqty     = $row['total_qty'];
+                $prod_code = $row['sysprocd'];
+                if (empty($trqty)){$trqty = 0;}
+                
+                if ($k % $shardSize == 0) {
+                    if ($k != 0) {
+                        mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+                    }
+                    $sqliq = 'Insert Into tmpfgmove (usernm, prodcd, openqty, issqty, recqty, adjqty, invoiceqty) Values ';
+                }
+                $sqliq .= (($k % $shardSize == 0) ? '' : ', ') . "('$var_loginid', '$prod_code', '$oqty', '$iqty',
+   			                                                   '$rqty', '$aqty', '$trqty')";
+                $k = $k + 1;
+                
+            }
+            if (!empty($sqliq)){
+                mysql_query($sqliq) or die("Unable Save In Temp Table ".mysql_error());
+            }
+            
+        } // end if nodata=0
+        
+        $fname = "fg_moverpt.rptdesign&__title=myReport";
+        $fname .= "&fd=".$fdte."&td=".$tdte;
+        $fname .= "&fpr=".$fjid."&tpr=".$tjid;
+        
+        $dest = "http://".$var_prtserver.":8080/birt-viewer/frameset?__report=".$fname."&usernm=".$var_loginid."&dbsel=".$varrpturldb;
+        $dest .= urlencode(realpath($fname));
+        
+        //header("Location: $dest" );
+        echo "<script language=\"javascript\">window.open('".$dest."','name','height=1000,width=1000,left=200,top=200');</script>";
+        //$backloc = "../prod_rpt/fg_moverpt.php?menucd=".$var_menucode;
+        //echo "<script>";
+        //echo 'location.replace("'.$backloc.'")';
+        //echo "</script>";
+        
+    }
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
